10db74442c93b9812a5e8f20c7a21cd2bc6eff78
Carlos <Nitro@localhost.localdomain>
2024-02-28 21:00:34 +0100

    peticion get para recuperar lista y pokemons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PokeAPI';
+  URL_LISTA_POKEMON: string = 'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    console.log(this.URL_LISTA_POKEMON);
+
+    // http.get nos devuelve una peticion asincrona (no sabemos cuando se ejecuta)
+    const peticionGet = this.http.get(this.URL_LISTA_POKEMON);
+
+    // nos suscribimos a ella pasandole la funcion que queremos ejecutar cuando se resuelva (ahora o en 1 año)
+    peticionGet.subscribe((respuesta: any) => {
+
+      for (let i = 0; i < respuesta.results.length ; i++) {
+        const url = respuesta.results[i].url;
+
+        this.http.get(url).subscribe((pokemon: any) => {
+          console.log("Nombre: " + pokemon.name + ". Movimiento favorito: " + pokemon.moves[0].move.name);
+        });
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; /* Importado aquí y en el AppComponent */
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
