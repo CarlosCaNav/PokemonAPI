780a44bdb5b8a1904c1482f4bfa7e000f2d92864
Carlos <Nitro@localhost.localdomain>
2024-03-01 15:14:29 +0100

    Un pasito más

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,17 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'PokeAPI';
 
-  pokeUnodddd = "url(https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png)";
-  pokeUno: Array <string> = [];
+  /*  pokeUnodddd = "url(https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png)"; */
+  pokeindex: Array<number> = [];
+  pokeIMG: Array<string> = [];
   URL_LISTA_POKEMON: string = 'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0';
   pokedex: boolean = false;
-  pokedexDatos: string = "";
+  pokedexDatos: string = "pito";
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-  
-   /*  console.log(this.URL_LISTA_POKEMON); */
+
+    /*  console.log(this.URL_LISTA_POKEMON); */
 
     // http.get nos devuelve una peticion asincrona (no sabemos cuando se ejecuta)
     const peticionGet = this.http.get(this.URL_LISTA_POKEMON);
@@ -26,27 +27,40 @@ export class AppComponent {
     // nos suscribimos a ella pasandole la funcion que queremos ejecutar cuando se resuelva (ahora o en 1 año)
     peticionGet.subscribe((respuesta: any) => {
 
-      for (let i = 0; i < 150 ; i++) { /* respuesta.results.length  */
-      var numeroAleatorio =  Math.floor(Math.random() * (150));
+      for (let i = 0; i < 4; i++) { /* respuesta.results.length  */
+        var numeroAleatorio = Math.floor(Math.random() * (150));
         const url = respuesta.results[numeroAleatorio].url;
-      /*   console.log("Holi" + respuesta.results[i].sprites.front_default); */
+        /*   console.log("Holi" + respuesta.results[i].sprites.front_default); */
 
-      //  this.pokeUno[i] = "url(" + respuesta.results.sprites.front_default + ")"
-     //   console.log(this.pokeUno);
+        //  this.pokeUno[i] = "url(" + respuesta.results.sprites.front_default + ")"
+        //   console.log(this.pokeUno);
 
         this.http.get(url).subscribe((pokemon: any) => {
-          this.pokeUno[i] = "url(" + pokemon.sprites.front_default + ")";
+          this.pokeIMG[i] = "url(" + pokemon.sprites.front_default + ")";
+          this.pokeindex[i] = numeroAleatorio;
+          console.log(this.pokeIMG);
+          console.log(this.pokeindex);
+          console.log(url);
+          /* 
+                    console.log("Nombre: " + pokemon.name + ". Movimiento favorito: " + pokemon.moves[0].move.name);
           
-          console.log("Nombre: " + pokemon.name + ". Movimiento favorito: " + pokemon.moves[0].move.name); 
-          
-          console.log(this.pokeUno); 
+                    console.log(this.pokeIMG); */
 
-        }); 
+        });
       }
     });
   }
-  info(){
-    this.pokedex = true;
+  info(indice: number) {
+    var PokemonElegido = this.pokeindex[indice]
 
+    if (this.pokedex == true) {
+      this.pokedex = false
+    } else { this.pokedex = true };
+
+    this.http.get(this.URL_LISTA_POKEMON)
+      .subscribe((respuesta: any) => {
+        console.log(`Nombre del primer Pokémon: ${respuesta.results[PokemonElegido].name}`);
+        this.pokedexDatos = `Nombre del primer Pokémon: ${respuesta.results[PokemonElegido].name}`
+      });
   }
 }
