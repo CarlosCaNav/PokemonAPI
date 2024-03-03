d73a0eb5467cba42af84f7579e2d49975347147d
Carlos <Nitro@localhost.localdomain>
2024-03-03 21:04:01 +0100

    Se añadió movimiento a los 4 pokemons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,73 +9,103 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'PokeAPI';
 
-  /*  pokeUnodddd = "url(https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png)"; */
+  bienvenida: boolean = true;
   pokeindex: Array<number> = [];
   pokeIMG: Array<string> = [];
   URL_LISTA_POKEMON: string = 'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0';
   pokedex: boolean = false;
   pokedexDatos: string = "pito";
+  posicionPokemonsX: Array<string> = [];
+  posicionPokemonsY: Array<string> = [];
+  pokemonScale: Array<number> = [];
+
 
   constructor(private http: HttpClient) { }
 
 
-  ngOnInit() {
-   
-  const top = Math.floor(Math.random() * (window.innerHeight - 500));
-  const left = Math.floor(Math.random() * window.innerWidth);
+  empezar() {
 
-  const div = document.getElementById('PokeMovimiento')!; // Operador de afirmación no nula
+this.bienvenida=false;
 
-  div.style.top = `${top}px`;
-  div.style.left = `${left}px`;
-  div.style.scale = `${top / window.innerHeight}`;
+    for (let i = 0; i <= 60; ++i) {
+      setTimeout(() => {
+      for (let j = 0; j <= this.pokeindex.length; ++j) {
 
-  setInterval(this.ngOnInit, 1000); /* soy un terrorista, seguro que hay formas mejores */
-   
+      setTimeout(() => {
 
-    for (let i = 0; i < 5; i++) { /* respuesta.results.length  */
-    var numeroAleatorio = Math.floor(Math.random() * (150));
-    this.pokeindex[i] = numeroAleatorio;
-  
+        const top = Math.floor(Math.random() * (window.innerHeight - 250));
+        const left = Math.floor(Math.random() * (window.innerWidth - 250));
+
+        this.posicionPokemonsX[j] = left + "px";
+        this.posicionPokemonsY[j] = top + "px";
+        this.pokemonScale[j] = top / window.innerHeight;
+
+      }, 250 * j);
+    }
+  }, 1000 * i)
+      /* setInterval(this.empezar, 1000);  *//* soy un terrorista, seguro que hay formas mejores */
+    }
   }
-    /*  console.log(this.URL_LISTA_POKEMON); */
 
-    // http.get nos devuelve una peticion asincrona (no sabemos cuando se ejecuta)
-    const peticionGet = this.http.get(this.URL_LISTA_POKEMON);
+    ngOnInit() {
 
-    // nos suscribimos a ella pasandole la funcion que queremos ejecutar cuando se resuelva (ahora o en 1 año)
-    peticionGet.subscribe((respuesta: any) => {
+      /*   const top = Math.floor(Math.random() * (window.innerHeight - 500));
+        const left = Math.floor(Math.random() * window.innerWidth);
+      
+        const div = document.getElementById('PokeMovimiento')!; // Operador de afirmación no nula
+      
+        div.style.top = `${top}px`;
+        div.style.left = `${left}px`;
+        div.style.scale = `${top / window.innerHeight}`;
+      
+        setInterval(this.ngOnInit, 1000);  *//* soy un terrorista, seguro que hay formas mejores */
 
-      for (let i = 0; i < 4; i++) { /* respuesta.results.length  */
-        const url = respuesta.results[this.pokeindex[i]].url;
 
+      for (let i = 0; i <= 5; i++) { /* respuesta.results.length  */
+        var numeroAleatorio = Math.floor(Math.random() * (150));
+        this.pokeindex[i] = numeroAleatorio;
 
-
-        /*   console.log("Holi" + respuesta.results[i].sprites.front_default); */
-
-        //  this.pokeUno[i] = "url(" + respuesta.results.sprites.front_default + ")"
-        //   console.log(this.pokeUno);
-
-        this.http.get(url).subscribe((pokemon: any) => {
-          this.pokeIMG[i] = "url(" + pokemon.sprites.front_default + ")";
-          /* 
-                    console.log("Nombre: " + pokemon.name + ". Movimiento favorito: " + pokemon.moves[0].move.name);
-          
-                    console.log(this.pokeIMG); */
-
-        });
       }
-    });
-  }
-  info(indice: number) {
-    var PokemonElegido = this.pokeindex[indice]
+      /*  console.log(this.URL_LISTA_POKEMON); */
 
-    this.pokedex = !this.pokedex;
+      // http.get nos devuelve una peticion asincrona (no sabemos cuando se ejecuta)
+      const peticionGet = this.http.get(this.URL_LISTA_POKEMON);
 
-    this.http.get(this.URL_LISTA_POKEMON)
-      .subscribe((respuesta: any) => {
-        this.pokedexDatos = `Éste Pokemon se llama: ${respuesta.results[PokemonElegido].name}`
+      // nos suscribimos a ella pasandole la funcion que queremos ejecutar cuando se resuelva (ahora o en 1 año)
+      peticionGet.subscribe((respuesta: any) => {
+
+        for (let i = 0; i <= 4; i++) { /* respuesta.results.length  */
+          const url = respuesta.results[this.pokeindex[i]].url;
+
+
+
+          /*   console.log("Holi" + respuesta.results[i].sprites.front_default); */
+
+          //  this.pokeUno[i] = "url(" + respuesta.results.sprites.front_default + ")"
+          //   console.log(this.pokeUno);
+
+          this.http.get(url).subscribe((pokemon: any) => {
+            this.pokeIMG[i] = "url(" + pokemon.sprites.front_default + ")";
+            /* 
+                      console.log("Nombre: " + pokemon.name + ". Movimiento favorito: " + pokemon.moves[0].move.name);
+            
+                      console.log(this.pokeIMG); */
+
+          });
+        }
       });
+    }
+    info(indice: number) {
+      var PokemonElegido = this.pokeindex[indice]
+
+      this.pokedex = !this.pokedex;
+
+
+
+      this.http.get(this.URL_LISTA_POKEMON)
+        .subscribe((respuesta: any) => {
+          this.pokedexDatos = `Éste Pokemon se llama: ${respuesta.results[PokemonElegido].name}`
+        });
+    }
+
   }
-  
-}
