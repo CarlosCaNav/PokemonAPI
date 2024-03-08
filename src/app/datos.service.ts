5f580773e4c36a27eb3a7e76e5f674a4f1799265
CarlosCaNav <142988262+CarlosCaNav@users.noreply.github.com>
2024-03-08 11:45:34 +0100

    Update src/app/datos.service.ts
    Co-authored-by: Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>

--- src/app/datos.service.ts
+++ src/app/datos.service.ts
@@ -25,7 +25,9 @@ export class DatosService {
       var numeroAleatorio = Math.floor(Math.random() * (150)); /* en este caso son 150 Pokemons los que queremos indexar. Pero se pueden muchísimos más */
       this.http.get(this.URL_LISTA_POKEMON)
         .subscribe((respuesta: any) => {
-          this.listaPokemonsVisibles[i].nombre = respuesta.results[numeroAleatorio]?.name.charAt(0).toUpperCase() + respuesta.results[numeroAleatorio]?.name.substring(1);
+          this.listaPokemonsVisibles[i] = { 
+            nombre: respuesta.results[numeroAleatorio]?.name.charAt(0).toUpperCase() + respuesta.results[numeroAleatorio]?.name.substring(1),
+         };
           
           console.log(this.listaPokemonsVisibles[i]);
           
